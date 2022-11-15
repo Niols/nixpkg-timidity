08acb50208857a6d5f0e45d4536a375a6f088c9c
Niols <niols@niols.fr>
2022-11-16 00:39:11 +0100

    Also provide timidity from nixpkgs

--- flake.nix
+++ flake.nix
@@ -6,8 +6,20 @@
 
       let pkgs = nixpkgs.legacyPackages.${system}; in
       {
-        packages.default = self.packages.${system}.timidity;
-        packages.timidity = pkgs.callPackage ./timidity {
+        packages.default = self.packages.${system}.timidityFromNixpkgs;
+
+        ## This is TiMidity++ as provided by Nixpkgs.
+        packages.timidityFromNixpkgs = pkgs.timidity;
+
+        ## This is TiMidity++ as provided by Nixpkgs, except we do the
+        ## `callPackage` manually here to show how it is done.
+        packages.timidityFromNixpkgs' = pkgs.callPackage ./timidity {
+          inherit (pkgs.darwin.apple_sdk.frameworks) CoreAudio;
+        };
+
+        ## This is TiMidity++ with additional vorbis support. Note the extra
+        ## argument to `callPackage`.
+        packages.timidityWithVorbis = pkgs.callPackage ./timidity {
           inherit (pkgs.darwin.apple_sdk.frameworks) CoreAudio;
           enableVorbis = true;
         };
